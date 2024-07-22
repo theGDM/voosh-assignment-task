5b7c2a97afff499aa5b920c868556504cea919f7
Gyandeep Mehra <gyandeepmehra370@gmail.com>
2024-07-22 17:24:57 +0530

    Gyandeep | Added api endpoint for login, register, get all tasks, create, delete, update task

---
+++ src/services/api.js
@@ -0,0 +1,74 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+const baseURL = 'https://task-manager-service-c5ko.onrender.com';
+
+export const register = async (fullName, email, password) => {
+    try {
+        let response = await axios.post(`${baseURL}/api/auth/register`, {
+            fullName: fullName,
+            email: email,
+            password: password
+        });
+
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export const signIn = async (email, password) => {
+    try {
+        let response = await axios.post(`${baseURL}/api/auth/login`, {
+            email: email,
+            password: password
+        });
+        return response.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export const createTask = async (userId, title, description, deadline) => {
+    try {
+        let response = await axios.post(`${baseURL}/api/tasks/${userId}`, {
+            title: title,
+            description: description,
+            taskDeadline: deadline,
+        });
+        toast('Task created successfully!');
+    } catch (err) {
+        console.log(err);
+        toast('Some error occured while creating the task!');
+    }
+}
+
+export const updateTask = async (taskId, body) => {
+    try {
+        let response = await axios.put(`${baseURL}/api/tasks/${taskId}`, body);
+        toast('Task updated successfully!');
+    } catch (err) {
+        console.log(err);
+        toast('Some error occured while upadting the task!');
+    }
+}
+
+export const deleteTask = async (taskId) => {
+    try {
+        let response = await axios.delete(`${baseURL}/api/tasks/${taskId}`);
+        toast('Task deleted successfully created!');
+    } catch (err) {
+        console.log(err);
+        toast('Some error occured while deleting the task!');
+    }
+}
+
+
+export const getAllTasks = async (userId) => {
+    try {
+        let response = await axios.get(`${baseURL}/api/tasks/${userId}/task`);
+        return response;
+    } catch (err) {
+        console.log(err);
+        toast('Some error occured while deleting the task!');
+    }
+} 
